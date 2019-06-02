a3e2d5cf2e00721e35e816ae9af9cc518ccb5b19
misseswaddles <carol.farris2@gmail.com>
2019-06-02 12:50:32 -0700

    Updated Songs.cs
    Updated Songs.cs with comments to describe what they are and what items are needed to add.

--- ListMusicFiles/ListMusicFiles/Songs.cs
+++ ListMusicFiles/ListMusicFiles/Songs.cs
@@ -8,8 +8,20 @@ namespace ListMusicFiles
 {
     public class Songs
     {
-        public string Title { get; set; }
-        public string Album { get; set; }
-        public string TitleName { get; set; }
+        public string Title { get; set; } //Definition: song title
+        public string Album { get; set; } //Definition: song album, add default
+        public string TitleName { get; set; } //Definition: song File Name 
+
+
+
+        //We need to implement the following:
+
+        //public string song file path
+        //public string song picture file path  (thumbnail image property)
+
+        //method with if/else to check for song name, 
+        //if false, label unknown name.
+
+
     }
 }
